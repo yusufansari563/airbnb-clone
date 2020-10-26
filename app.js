599b622d520f0fe304af13027f34d4c4dc0a25d3
yusufansari563 <yusufansari563@gmail.com>
2020-10-26 10:44:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const { func } = require("prop-types");
+
+const { number } = require("prop-types")
+
+// let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+// let response = fetch(url);
+
+// let commits = response.json(); // read response body and parse as JSON
+
+// console.log(commits[0].author.login);
+
+@param {number} a 
+@param {number} a
+
+function mul(a,b) {
+  return a * b
+}
